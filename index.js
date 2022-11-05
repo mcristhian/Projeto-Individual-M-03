946c3bf6f071034e877175ba933f9181230b514c
mcristhian <thematheuscris@gmail.com>
2022-11-04 21:24:18 -0300

    Teste

--- index.js
+++ index.js
@@ -1,73 +1,59 @@
-const g = require('axios');
-const po = require('axios');
-const pu = require('axios');
-const d = require('axios');
-// let arg = process.argv.slice(2);
-let g_arg = 'rotas';
+const data = require("./db.json");
 
-let po_arg = 'planos';
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-let pu_arg = 'rotas';
-let pu_arg_id = 2;
-
-let d_arg = 'planos';
-let d_arg_id = 4;
+server.use(middlewares);
+server.use(router);
 
 //Get
-function f_get(){
-    g.get(`https://projeto-individual-m-03.onrender.com/${g_arg}`)
-    .then(resp_g => {
-        data = resp_g.data;
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(`O seguinte erro aconteceu: ${error}`);
-    });
-}
+server.get('/servicos', (req, res) => {
+
+    return res.json(data.servicos)
+});
 
 //Post
-function f_post(){
-    po.post(`https://projeto-individual-m-03.onrender.com/${po_arg}`, {
-    id: 6,
-    name: "Avulso 3",
-    description: "Plano alternativo",
-    price: "R$ 55.000"
-    })
-    .then(resp_po => {
-        console.log('Adicionado.');
-    })
-    .catch(error => {
-        console.log(`O seguinte erro aconteceu: ${error}`);
-    });
-}
+server.post('/servicos', (req, res) => {
+    const { name } = req.body;
+    data.servicos.push(name);
 
+    return res.json(data.servicos);
+});
 
 //Put
-function f_put(){
-    pu.put(`https://projeto-individual-m-03.onrender.com/${pu_arg}/${pu_arg_id}`, {
-    // id: 1,
-    name: "RJ x VT",
-    description: 'Trajeto médio.',
-    distance: "436,85 km"
-    })
-    .then(resp_pu => {
-        console.log('Modificado.')
-    })
-    .catch(error => {
-        console.log(`O seguinte erro aconteceu: ${error}`);
-    });
-}
+server.put('/servicos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
 
+    data.servicos[index] = name;
+
+    return res.json(data.servicos);
+});
 
 //Delete
-function f_delete(){
-    d.delete(`https://projeto-individual-m-03.onrender.com/${d_arg}/${d_arg_id}`)
-    .then(resp_d => {
-        console.log('Deletado.');
-    })
-    .catch(error => {
-        console.log(`O seguinte erro aconteceu: ${error}`);
-    });
-}
+server.delete('/servicos/:index', (req, res) => {
+    const { index } = req.params;
 
-f_get();
+    data.splice(index, 1);
+    return res.json({ message: "O item foi deletado" });
+})
+
+server.delete("/servicos/:index", (req, res) => {
+    const item = item.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Error: Item não foi apagada com sucesso!"
+        });
+        return res.json({
+            error: false,
+            message: "Artigo apagado com sucesso!"
+        });
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running in http://localhost:${port}`);
+});
